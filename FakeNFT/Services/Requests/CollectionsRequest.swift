007b350f183fa8a0a4a50106e26f2f5d61d81029
RespectRus <respectspbrus@yandex.ru>
2023-10-11 15:25:17 +0300

    no message

--- FakeNFT/Services/Requests/CollectionsRequest.swift
+++ FakeNFT/Services/Requests/CollectionsRequest.swift
@@ -1,8 +1,7 @@
-//
-//  CollectionsRequest.swift
-//  FakeNFT
-//
-//  Created by Respect on 11.10.2023.
-//
-
 import Foundation
+
+struct CollectionsRequest: NetworkRequest {
+    var endpoint: URL? {
+        URL(string: "https://651ff107906e276284c3c2d0.mockapi.io/api/v1/collections")
+    }
+}
