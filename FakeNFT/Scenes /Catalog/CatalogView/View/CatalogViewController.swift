a69e98a6d370b3d9f72e4c3658a8092bff53f8a5
RespectRus <respectspbrus@yandex.ru>
2023-10-12 12:19:43 +0300

    В CatalogViewController добавлено: 1. Настройки catalogTableView; 2. Класс CatalogViewController проинициализирован; 3. Добалены расширения, CatalogViewController подписан на UITableViewDataSource и UITableViewDelegate; 4. Добавлен алерт для обработки ошибки.

--- FakeNFT/Scenes /Catalog/CatalogView/View/CatalogViewController.swift
+++ FakeNFT/Scenes /Catalog/CatalogView/View/CatalogViewController.swift
@@ -11,10 +11,57 @@ final class CatalogViewController: UIViewController {
        let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(CatalogTableViewCell.self, forCellReuseIdentifier: CatalogTableViewCell.identifier)
+        tableView.backgroundColor = .background
+        tableView.separatorColor = .background
+        tableView.dataSource = self
+        tableView.delegate = self
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    init(viewModel: CatalogViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        viewModel.onChange = self.catalogTableView.reloadData
+        viewModel.onError = { [weak self] error in
+            self?.showErrorAlert(error: error)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func showErrorAlert(error: String) {
+        let alertController = UIAlertController(
+            title: "alertErrorTitle",
+            message: error,
+            preferredStyle: .alert
+        )
+        alertController.addAction(UIAlertAction(title: "no", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "repeat", style: .default, handler: { [weak self] _ in
+            guard let self = self else { return }
+            self.viewModel.updateData()
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension CatalogViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension CatalogViewController: UITableViewDelegate {
+    
 }
