6f4be633f046b63600adb2d382db1d685a0279ac
Artem <kohanartem@gmail.com>
2023-10-12 18:52:56 +0300

    Отформатированный код и актуализирован markdown

--- FakeNFT/Scenes /Profile/AboutDeveloper/AboutDeveloperViewController.swift
+++ FakeNFT/Scenes /Profile/AboutDeveloper/AboutDeveloperViewController.swift
@@ -15,49 +15,54 @@ final class AboutDeveloperViewController: UIViewController, WKNavigationDelegate
         webView.backgroundColor = UIColor(named: "ypWhite")
         return webView
     }()
-
+    
     private lazy var customBackButton: UIBarButtonItem = {
-        let uiBarButtonItem = UIBarButtonItem(image: UIImage(named: "BackButton"), style: .plain, target: self, action: #selector(goBack))
+        let uiBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "BackButton"),
+            style: .plain,
+            target: self,
+            action: #selector(goBack)
+        )
         return uiBarButtonItem
     }()
-
+    
     private var profileLink: String?
-
+    
     init(profileLink: String?) {
         self.profileLink = profileLink
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupView()
         setupHierarchy()
         setupLayout()
-
+        
         guard let profileLink = profileLink,
               let url = URL(string: profileLink) else { return }
         let request = URLRequest(url: url)
-
+        
         webView.load(request)
     }
-
+    
     private func setupView() {
         view.backgroundColor = UIColor(named: "ypWhite")
         webView.navigationDelegate = self
-
+        
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationItem.leftBarButtonItem = customBackButton
     }
-
+    
     private func setupHierarchy() {
         view.addSubview(webView)
     }
-
+    
     private func setupLayout() {
         NSLayoutConstraint.activate([
             webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -66,7 +71,7 @@ final class AboutDeveloperViewController: UIViewController, WKNavigationDelegate
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
-
+    
     @objc
     func goBack() {
         navigationController?.popViewController(animated: true)
--- FakeNFT/Scenes /Profile/ProfileEdition/ProfileEditionViewController.swift
+++ FakeNFT/Scenes /Profile/ProfileEdition/ProfileEditionViewController.swift
@@ -10,7 +10,7 @@ import Kingfisher
 
 final class ProfileEditionViewController: UIViewController {
     private let viewModel: ProfileViewModel
-
+    
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.contentSize = CGSize(width: view.bounds.width, height: 1000)
@@ -18,7 +18,7 @@ final class ProfileEditionViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
     }()
-
+    
     private lazy var closeProfileButton: UIButton = {
         let closeProfileButton = UIButton.systemButton(
             with: UIImage(named: "closeProfile") ?? UIImage(),
@@ -26,20 +26,20 @@ final class ProfileEditionViewController: UIViewController {
             action: #selector(didTapCloseProfileButton))
         closeProfileButton.tintColor = .black
         closeProfileButton.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return closeProfileButton
     }()
-
+    
     private let profilePictureImage: UIImageView = {
         let profilePictureImage = UIImageView()
         profilePictureImage.layer.cornerRadius = 35
         profilePictureImage.layer.masksToBounds = true
         profilePictureImage.translatesAutoresizingMaskIntoConstraints = false
         profilePictureImage.image = UIImage(named: "MockUserPic")
-
+        
         return profilePictureImage
     }()
-
+    
     private let changePictureLabel: UILabel = {
         let changePictureLabel = UILabel()
         changePictureLabel.text = "Сменить фото"
@@ -51,29 +51,29 @@ final class ProfileEditionViewController: UIViewController {
         changePictureLabel.translatesAutoresizingMaskIntoConstraints = false
         changePictureLabel.numberOfLines = 2
         changePictureLabel.textAlignment = .center
-
+        
         return changePictureLabel
     }()
-
+    
     private let profileImageLinkLabel: UILabel = {
         let profileImageLinkLabel = UILabel()
         profileImageLinkLabel.text = "Загрузить изображение"
         profileImageLinkLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
         profileImageLinkLabel.translatesAutoresizingMaskIntoConstraints = false
         profileImageLinkLabel.isHidden = true
-
+        
         return profileImageLinkLabel
     }()
-
+    
     private let nameLabel: UILabel = {
         let nameLabel = UILabel()
         nameLabel.text = "Имя"
         nameLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return nameLabel
     }()
-
+    
     private lazy var nameTextField: UITextField = {
         let nameTextField = TextField()
         nameTextField.placeholder = "Введите имя"
@@ -83,19 +83,19 @@ final class ProfileEditionViewController: UIViewController {
         nameTextField.clearButtonMode = .whileEditing
         nameTextField.translatesAutoresizingMaskIntoConstraints = false
         nameTextField.tintColor = UIColor(named: "iconBlue")
-
+        
         return nameTextField
     }()
-
+    
     private let descriptionLabel: UILabel = {
         let descriptionLabel = UILabel()
         descriptionLabel.text = "Описание"
         descriptionLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return descriptionLabel
     }()
-
+    
     private lazy var descriptionTextField: UITextField = {
         let descriptionTextField = TextField()
         descriptionTextField.placeholder = "Введите описание"
@@ -105,19 +105,19 @@ final class ProfileEditionViewController: UIViewController {
         descriptionTextField.clearButtonMode = .whileEditing
         descriptionTextField.translatesAutoresizingMaskIntoConstraints = false
         descriptionTextField.tintColor = UIColor(named: "iconBlue")
-
+        
         return descriptionTextField
     }()
-
+    
     private let websiteLabel: UILabel = {
         let websiteLabel = UILabel()
         websiteLabel.text = "Сайт"
         websiteLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
         websiteLabel.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return websiteLabel
     }()
-
+    
     private lazy var websiteTextField: UITextField = {
         let websiteTextField = TextField()
         websiteTextField.placeholder = "Укажите ссылку"
@@ -127,22 +127,22 @@ final class ProfileEditionViewController: UIViewController {
         websiteTextField.clearButtonMode = .whileEditing
         websiteTextField.translatesAutoresizingMaskIntoConstraints = false
         websiteTextField.tintColor = UIColor(named: "iconBlue")
-
+        
         return websiteTextField
     }()
-
+    
     init(viewModel: ProfileViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(keyboardWillShow),
@@ -155,33 +155,33 @@ final class ProfileEditionViewController: UIViewController {
             name: UIResponder.keyboardWillHideNotification,
             object: nil
         )
-
+        
         setupView()
         setupHierarchy()
         setupLayout()
     }
-
+    
     private func setupView() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(changePictureDidTap))
-
+        
         changePictureLabel.addGestureRecognizer(tapGesture)
         changePictureLabel.isUserInteractionEnabled = true
-
+        
         view.backgroundColor = UIColor(named: "ypWhite")
         self.navigationController?.navigationBar.isHidden = true
         guard let currentProfile = viewModel.profile else { return }
-
+        
         profilePictureImage.kf.setImage(
             with: URL(string: currentProfile.avatar),
             placeholder: UIImage(named: "MockUserPic"),
             options: [.processor(RoundCornerImageProcessor(cornerRadius: 35))])
-
+        
         nameTextField.text = currentProfile.name
         descriptionTextField.text = currentProfile.description
         websiteTextField.text = currentProfile.website
         profileImageLinkLabel.text = currentProfile.avatar
     }
-
+    
     private func setupHierarchy() {
         scrollView.addSubview(closeProfileButton)
         scrollView.addSubview(profilePictureImage)
@@ -193,43 +193,43 @@ final class ProfileEditionViewController: UIViewController {
         scrollView.addSubview(descriptionTextField)
         scrollView.addSubview(websiteLabel)
         scrollView.addSubview(websiteTextField)
-
+        
         view.addSubview(scrollView)
     }
-
+    
     private func setupLayout() {
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-
+            
             closeProfileButton.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 30),
             closeProfileButton.trailingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor,
                 constant: -Constants.defaultOffset),
             closeProfileButton.heightAnchor.constraint(equalToConstant: 42),
             closeProfileButton.widthAnchor.constraint(equalToConstant: 42),
-
+            
             profilePictureImage.topAnchor.constraint(equalTo: closeProfileButton.bottomAnchor, constant: 22),
             profilePictureImage.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             profilePictureImage.heightAnchor.constraint(equalToConstant: Constants.imageSize),
             profilePictureImage.widthAnchor.constraint(equalToConstant: Constants.imageSize),
-
+            
             changePictureLabel.centerXAnchor.constraint(equalTo: profilePictureImage.centerXAnchor),
             changePictureLabel.centerYAnchor.constraint(equalTo: profilePictureImage.centerYAnchor),
             changePictureLabel.widthAnchor.constraint(equalToConstant: Constants.imageSize),
             changePictureLabel.heightAnchor.constraint(equalToConstant: Constants.imageSize),
-
+            
             profileImageLinkLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             profileImageLinkLabel.topAnchor.constraint(equalTo: changePictureLabel.bottomAnchor, constant: 12),
-
+            
             nameLabel.topAnchor.constraint(equalTo: profilePictureImage.bottomAnchor, constant: 24),
             nameLabel.leadingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor,
                 constant: Constants.defaultOffset),
             nameLabel.heightAnchor.constraint(equalToConstant: 28),
-
+            
             nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
             nameTextField.leadingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor,
@@ -238,13 +238,13 @@ final class ProfileEditionViewController: UIViewController {
                 equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor,
                 constant: -Constants.defaultOffset),
             nameTextField.heightAnchor.constraint(equalToConstant: 44),
-
+            
             descriptionLabel.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 24),
             descriptionLabel.leadingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor,
                 constant: Constants.defaultOffset),
             descriptionLabel.heightAnchor.constraint(equalToConstant: 22),
-
+            
             descriptionTextField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8),
             descriptionTextField.leadingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor,
@@ -253,13 +253,13 @@ final class ProfileEditionViewController: UIViewController {
                 equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor,
                 constant: -Constants.defaultOffset),
             descriptionTextField.heightAnchor.constraint(equalToConstant: 132),
-
+            
             websiteLabel.topAnchor.constraint(equalTo: descriptionTextField.bottomAnchor, constant: 24),
             websiteLabel.leadingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor,
                 constant: Constants.defaultOffset),
             websiteLabel.heightAnchor.constraint(equalToConstant: 22),
-
+            
             websiteTextField.topAnchor.constraint(equalTo: websiteLabel.bottomAnchor, constant: 8),
             websiteTextField.leadingAnchor.constraint(
                 equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor,
@@ -270,7 +270,7 @@ final class ProfileEditionViewController: UIViewController {
             websiteTextField.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
-
+    
     @objc
     private func didTapCloseProfileButton() {
         viewModel.putProfile(
@@ -281,30 +281,30 @@ final class ProfileEditionViewController: UIViewController {
             likes: viewModel.profile?.likes ?? [""])
         dismiss(animated: true)
     }
-
+    
     @objc
     private func changePictureDidTap() {
         profileImageLinkLabel.text = viewModel.profile?.avatar
         profileImageLinkLabel.isHidden = false
-
+        
         let alertController = UIAlertController(
             title: "Введите ссылку на изображение",
             message: nil,
             preferredStyle: .alert)
-
+        
         alertController.addTextField { textField in
             textField.placeholder = "Ссылка на изображение"
         }
-
+        
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
-
+        
         let okAction = UIAlertAction(title: "ОК", style: .default) { [weak self] _ in
             if let textField = alertController.textFields?.first,
                let imageURLString = textField.text,
                let imageURL = URL(string: imageURLString),
                UIApplication.shared.canOpenURL(imageURL) {
                 self?.profileImageLinkLabel.text = imageURLString
-
+                
                 self?.profilePictureImage.kf.setImage(
                     with: URL(string: imageURLString),
                     placeholder: UIImage(named: "MockUserPic"),
@@ -313,13 +313,13 @@ final class ProfileEditionViewController: UIViewController {
                 self?.showInvalidURLAlert()
             }
         }
-
+        
         alertController.addAction(cancelAction)
         alertController.addAction(okAction)
-
+        
         present(alertController, animated: true, completion: nil)
     }
-
+    
     func showInvalidURLAlert() {
         let alertController = UIAlertController(
             title: "Неверный формат ссылки",
@@ -329,14 +329,14 @@ final class ProfileEditionViewController: UIViewController {
         alertController.addAction(okAction)
         present(alertController, animated: true, completion: nil)
     }
-
+    
     @objc
     func keyboardWillShow(notification: Notification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
         }
     }
-
+    
     @objc
     func keyboardWillHide(notification: Notification) {
         scrollView.contentInset = UIEdgeInsets.zero
